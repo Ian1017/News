6e0e26eee05d1e319e1e3e48d4b638ccaf6cdc9a
ian1017 <mdawidamengo@gmail.com>
2020-01-07 14:51:34 +0300

    Add function that returns all news

--- app/main/views.py
+++ app/main/views.py
@@ -41,3 +41,19 @@ def headlines(per_page):
     title = 'Top Headlines'
 
     return render_template('topheadlines.html', title = 'Top Headlines', news = topheadlines_news)
+
+
+@main.route('/everything&<int:per_page')
+def all_news(per_page):
+    '''
+    Function that returns top headlines articles
+    '''
+    everything_news = everything(per_page)
+    title = 'All News'
+
+    search_articles = request.args.get('search_query')
+
+    if search_articles:
+        return redirect(url_for('main.search', topic = search_articles))
+    else:
+        return render_template('everything.html', title = title, name = 'All News', news = everything_news)
